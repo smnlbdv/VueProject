8327d213e0242de7f49dca6d7cc8716df027f487
Semen Lebedew <mamontedinorog@yandex.by>
2023-04-01 19:42:27 +0300

    dffdgfg

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="#app">
+    <user-card/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import UserCard from "./components/UserСard.vue";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    UserCard  
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
